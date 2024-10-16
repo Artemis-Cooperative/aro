747c7b0821032bca7670571e7ba49b197e719de3
Ryan Ogden <12yanogden@gmail.com>
2024-10-15 21:32:04 -0400

    add equals outline

--- operations/equals/equals.go
+++ operations/equals/equals.go
@@ -1,1 +1,23 @@
 package aro
+
+import (
+	"reflect"
+)
+
+func Equals[T any](a, b T) bool {
+	// type
+
+	// Get type of T
+
+	// If T is any
+	// 	type of a == type of b
+	// 		store type
+	// 	else
+	// 		return false
+	// else
+	// 	store type
+
+	// Comparable, DeepEqual, Struct, and Map?
+
+	return reflect.DeepEqual(a, b)
+}
